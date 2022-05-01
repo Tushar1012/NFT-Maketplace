5c756c15bd8cecab3aded7b27af8d84f46825256
Tushar <Tusharsolanki1011@gmail.com>
2022-05-02 03:09:35 +0530

    deployment done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,12 @@ module.exports = {
   theme: {
     extend: {},
   },
+  variants:{
+    extend:{},
+  },
   plugins: [],
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
 }
