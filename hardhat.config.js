64a0be44980e5651584eec4b3954e552b8213c9c
Tushar <Tusharsolanki1011@gmail.com>
2022-05-01 15:19:25 +0530

    marketplace compleate

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomiclabs/hardhat-waffle");
-
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString()
+const projectId ="cf738b15ff4c4754a718a5705b47a659"
 
 module.exports = {
   solidity: "0.8.4",
+  networks :{
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: { 
+      url:  `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      account :[privateKey]
+    },
+  }
 };
